bcc45046b98555e94b9defaf11c330cfdc47664f
RajulThakur <rajul.singh.ece.300723@adgitmdelhi.ac.in>
2025-04-15 17:58:11 +0530

    refactor: simplify Vite configuration by removing SSR and Rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,5 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    ssr: true,
-    rollupOptions: {
-      input: {client: './src/main.tsx', server: './src/main.server.tsx'},
-    },
-  },
-});
+// https://vite.dev/config/
+export default defineConfig({plugins: [react()]});
